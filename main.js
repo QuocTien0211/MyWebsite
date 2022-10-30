12b14ff355b3b3fe6cc7b86d436ecab0ef995f3a
QuocTien Nguyen <quoctien211@gmail.com>
2022-10-30 17:25:06 +0700

    Add new project myWebsite

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("Chào mọi người");
+var a=51;
+var b=123; 
+var c=a+b;
+var firstName = "Nguyễn Quốc ";
+var lastName = "Tiến";
+var d;
+if( a > b)
+    d = "chán lắm";
+else (a<b)
+    d = "bình thường";
+
+
+function showDialog( ) {
+    alert("hi");
+}
+//showDialog();
+//setInterval(showDialog,5000);
